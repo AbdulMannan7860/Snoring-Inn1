45d2da2118aa41390c9f8629cae04dd0f99892a3
SaskaSolutions <saskasolutions@gmail.com>
2024-09-25 19:14:20 +0500

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,11 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+    cors({
+        origin: `${process.env.CLIENT_URL}`,
+    })
+);
 
 connectToMongo()
     .then(() => {
